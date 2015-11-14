6fe2d89fd902a3e405d4919a89f04240e7d70892
Whelpley <whelpley@gmail.com>
2015-11-13 18:14:10 -0800

    Acheive MVP game for solo challenge 7.7

--- week-7/game.js
+++ week-7/game.js
@@ -31,9 +31,8 @@ play command prompts response from bear
 win/lose text
 */
 
-
-// Initial Code
-
+// MVP - one-turn Win/Lose guessing game
+/*
 // initial code to allow prompting
 var prompt = require('prompt');
 prompt.start();
@@ -47,18 +46,19 @@ var stack = [
   "boots and pants "
 ];
 
-// declaring a DJ character, with attributes for track being played, mauls received, and wiggles received
+// declaring a DJ character, with attributes for track being played, grumbles received, and wiggles received
 var dj = {
   current: "",
-  mauls: 0,
+  grumbles: 0,
   wiggles: 0,
 };
 
-// declaring a bear character, with a favorite record (as yet undecided)
+// declaring a bear character, with a favorite record (initially undecided)
 var bear = {
   fave: "",
 };
 
+// function to display tracks and what is currently playing
 function display() {
   console.log("Here are the tracks you can play:");
   for (var i = 0; i < stack.length; i++) {
@@ -69,50 +69,160 @@ function display() {
   }
 }
 
-function playGame() {
+// fcn to start the game and load the bear's favorite track
+function gameIntro() {
   console.log("Wow, what a sweet forest rave.");
   console.log("AHHH! A BEAR! Only a DJ can save us!");
   console.log("You have to play the right track to appease the ursine raver.");
-//choose random track from stack for bear's favorite
-  bear.fave = stack[Math.floor(Math.random()*stack.length)];
-
-
-
-// show tracks and prompt a selection by user
-// will have to account for non-friendly inputs
-// gotta get prompt working
+  bearChoose();
   display();
-  var selection = 1; // prompt("Choose a track by number");
-  dj.current = stack[selection];
+}
+
+//fcn to choose random track from stack for bear's favorite
+function bearChoose() {
+  bear.fave = stack[Math.floor(Math.random()*stack.length)];
+}
+
+// fcn to take input from prompt and calculate game conclusion
+function conclusion(err, result) {
+  dj.current = stack[result.track_choice];
   console.log("You lay down a fat track, and the speakers go: " + dj.current + dj.current + dj.current);
 
-// compare chosen track with bear's fave, consequences
-  if (dj.current == bear.fave) {
+  compareTracks(dj.current, bear.fave);
+
+  checkCounters(dj.wiggles, dj.grumbles);
+}
+
+// fcn to check tracks and update counters
+function compareTracks(track1, track2){
+  if (track1 == track2) {
     dj.wiggles++;
     console.log("The bear grunts in approval and wiggles its butt.");
   }
   else {
-    dj.mauls++;
-    console.log("The bear rumbles his discontent.");
+    dj.grumbles++;
+    console.log("The bear rumbles his discontent. Better watch out ...");
   }
+}
 
-// // final consequences
-  if (dj.wiggles == 1)
+// fcn to check counters & determine Win/Lose of game
+function checkCounters(wiggles, grumbles) {
+  if (wiggles == 1)
     console.log("The bear give you a big hug. You have saved the rave!");
-  else if (dj.mauls == 1)
-    console.log("The bear raises its claw and mauls you. This was not a good rave.")
+  else if (grumbles == 1)
+    console.log("The bear raises its claw and mauls you. This was not a good rave.");
+}
+
+// the script of the game
+function playGame() {
+  gameIntro();
+  prompt.get(['track_choice'], conclusion);
 };
 
-
 // Driver Code
+playGame();*/
 
-playGame();
-
-
-
+// ----------------
 // Refactored Code
 
+// Refactor log:
+// ... acheived ...
+// -collapse gameIntro text to one call
+// -raise limit on grumbles to 3 before mauling occurs, if neither Win/Lose acheived, cycles back to call
+// Full Game -
+// if wrong record, wiggles reset to 0
+// must acheive 2 wiggles to win
+// * added * sound * effects * to * playTurn * method *
+// if player chooses non-valid track, gets error message
+// rename bear.fave to bear.wants
+//...docket below...
+// Bear gets shuffled array of album order
+// bear gets pointer to current fave (starts empty)
 
+var prompt = require('prompt');
+prompt.start();
+
+var stack = [
+  "wub wub wub",
+  "oontz oontz oontz",
+  "doo wap doo wah",
+  "don\'t Snapchat that duckface",
+  "boots and pants"
+];
+
+var dj = {
+  current: "",
+  grumbles: 0,
+  wiggles: 0,
+};
+
+var bear = {
+  wants: "",
+};
+
+function gameIntro() {
+  console.log("Wow, what a sweet forest rave.\nAHHH! A BEAR! Only a DJ can save us!\nYou have to play the right track to appease the ursine raver.\nChoose wisely - if you keep making it mad, it might attack you!\nIf the bear likes a track, play it again!");
+  bearChoose();
+  display();
+}
+
+// change to build the bear's choice stack
+function bearChoose() {
+  bear.wants = stack[Math.floor(Math.random()*stack.length)];
+}
+
+function display() {
+  console.log("Here are the tracks you can play:");
+  for (var i = 0; i < stack.length; i++) {
+    if (stack[i] == dj.current)
+      console.log("Currently playing Track #" + i + ":" + stack[i]);
+    else
+      console.log("Track #" + i + ": " + stack[i]);
+  }
+}
+
+function playTurn(err, result) {
+  if (stack[result.track_choice] == undefined){
+    console.log("You fumble at the controls.");
+  }
+  else {
+    dj.current = stack[result.track_choice];
+    console.log("You lay down a fat track, and the speakers go: * " + dj.current + " * " + dj.current + " * " + dj.current + " *");
+  }
+  compareTracks(dj.current, bear.wants);
+  checkCounters(dj.wiggles, dj.grumbles);
+}
+
+function compareTracks(track1, track2){
+  if (track1 == track2) {
+    dj.wiggles++;
+    console.log("The bear grunts in approval and wiggles its butt.");
+  }
+  else {
+    dj.grumbles++;
+    dj.wiggles = 0;
+    console.log("The bear rumbles his discontent. Better watch out ...");
+  }
+}
+
+function checkCounters(wiggles, grumbles) {
+  if (wiggles == 2)
+    console.log("The bear gives you a big hug. You have saved the rave!");
+  else if (grumbles == 3)
+    console.log("The bear raises its claw and mauls you. This was not a good rave.");
+  else {
+    console.log("The bear is still on the dance floor. Pick another record!");
+    display();
+    prompt.get(['track_choice'], playTurn);
+  }
+}
+
+function playGame() {
+  gameIntro();
+  prompt.get(['track_choice'], playTurn);
+};
+
+playGame();
 
 
 
--- week-7/t2.js
+++ week-7/t2.js
@@ -2,18 +2,28 @@ prompt = require('prompt');
 prompt.start();
 
 
-villain = {
+var villain = {
   name: "Chad"
 };
 
+var hero = {
+  name: "Steve"
+}
 console.log(villain.name + " was here first");
 
-prompt.get(['newname'], function (err, result) {
+function test(err, result) {
   console.log('New name: ' + result.newname);
   villain.name = result.newname;
   console.log(villain.name + " is still a jerk.");
-});
+
+}
+
+prompt.get(['newname'], test);
 
 
-console.log(villain.name + " should be the new name");
+
+//if (empty != []) console.log(empty);
+
+
+//console.log(villain.name + " should be the new name");
 // but it's not! line 18 is executed before the prompt instead of after...
